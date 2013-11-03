171560f9812628d3c5ae81b6bb70818ef3c13d72
Partus <partus@mail.com>
2013-11-03 18:25:06 +0000

    slider css aded

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function(grunt) {
       },
       bootstrap: {
         src: ['less/bootstrap.less'],
-        dest: 'dist/css/<%= pkg.name %>.css'
+        dest: '/home/user/dev/multishop/avendor/<%= pkg.name %>.css'
       },
       min: {
         options: {
