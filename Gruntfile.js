6fe3e57c356b62829de008a3bfa4a09263c69f7b
Partus <partus@mail.com>
2014-06-26 18:40:17 +0400

    insight-framework init

--- Gruntfile.js
+++ Gruntfile.js
@@ -156,7 +156,7 @@ module.exports = function (grunt) {
           sourceMapFilename: 'dist/css/<%= pkg.name %>.css.map'
         },
         files: {
-          'dist/css/<%= pkg.name %>.css': 'less/bootstrap.less'
+          '/home/user/dev/insight/InSight-Multi-Modal/framework/client/lib/bootstrap/bootstrap.css': 'less/bootstrap.less'
         }
       },
       compileTheme: {
